be592398781c03ed28db9e618032e090661b09ad
Suckhodoev Artem <fetelupercal@gmail.com>
2025-09-13 15:00:13 +0500

    create producer

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+#include <time.h> 
+
+#define BUFFER_SIZE 10
 
 struct Task
 {
     /* data */
-    int completed;
     int time;
     int taskId;
+    int creatorId;
 };
 
-struct Task tasks[10];
+struct Task tasksBuffer[10];
+int count = 0; 
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; //  это специальный макрос, который компилятор заменяет на код, необходимый для инициализации мьютекса со значениями по умолчанию
 
@@ -20,10 +25,41 @@ int random_int(){
 void* producer(void* arg){
 
     //size_t length = sizeof(tasks) / sizeof(task[0]);
-    
+    int thread_num = *(int *)arg;
+
+    while (count < BUFFER_SIZE)
+    {
+        pthread_mutex_lock(&mutex);
+
+        sleep(1);
+
+        struct Task newTask = { 
+            random_int(), 
+            count + 1, 
+            thread_num 
+        };
+
+        tasksBuffer[count] = newTask;
+
+        count++;
+
+        pthread_mutex_unlock(&mutex);
+
+        printf("Поток %d: добавил задачу номер %d !\n", thread_num, newTask.taskId);
+    }
+
+    return NULL;
 }
 
-void *consumer(void *arg){
+void* consumer(void *arg){
+
+    while(1){
+        //pthread_mutex_lock(&mutex);
+        
+        
+
+        //pthread_mutex_unlock(&mutex);
+    }
 
 }
 
@@ -32,5 +68,13 @@ int main(){
 
     pthread_t prod_thread, cons_thread;
 
+    int thread_args_id = 1;
+    
+    pthread_create(&prod_thread, NULL, producer, &thread_args_id);
+    //pthread_create(&prod_thread, NULL, producer, 2);
 
+    pthread_join(prod_thread, NULL);
+    //pthread_join(cons_thread, NULL);
+    
+    return 0;
 }
